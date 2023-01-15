42ba473989c9928e43185a9121f8af575a1c1f79
dlee4912 <dlee4912@gmail.com>
2023-01-15 17:22:22 -0600

    Added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+# Read in the source dataset
 csvpath = os.path.join('Resources/budget_data.csv')
 
 with open(csvpath) as csvfile:
@@ -8,26 +9,34 @@ with open(csvpath) as csvfile:
 
     csv_header = next(csvreader)
 
+    # Create variables for total months and PL
     months = 0
-    totalProfitLoss = 0
+    totalProfitLoss = 0 
 
+    # Variables to store changes between months
     lastRowPL = 0
     totalMonthlyChanges = 0
 
+    # Varaibles for the greatest increase and decrease
     greatestInc = 0
     greatestIncDate = ""
     greatestDec = 0
     greatestDecDate = ""
 
+    # Iterate through each row of the csv
     for row in csvreader:
 
+        # Store the date and PL
         plDate = row[0]
         pl = int(row[1])
 
+        # Calculate total PL and add it to the total
         totalProfitLoss += pl
-
+        
+        # Compare the change between the last and current month's PL
         monthlyDiff = pl - lastRowPL
 
+        # Update greatest increase/decrease values
         if(monthlyDiff > greatestInc):
             greatestInc = monthlyDiff
             greatestIncDate = plDate
@@ -36,19 +45,24 @@ with open(csvpath) as csvfile:
             greatestDec = monthlyDiff
             greatestDecDate = plDate
 
+        # Store the value of the current row for the next iteration
         lastRowPL = pl
 
+        # Skip the first month's value because there is no previous month to compare the change
         if(months > 0):
             totalMonthlyChanges += monthlyDiff
-
+        
+        # Increment the month
         months += 1
 
-    netPLavg = round(float(totalMonthlyChanges)/(months-1), 2)
+# Calculate the average change in PL. One less month for the total because we skipped the first month. Round to 2 decimals.
+netPLavg = round(float(totalMonthlyChanges)/(months-1), 2)
 
-
+# Set the output path
 outputPath = os.path.join("analysis/output.txt")
 
 with open(outputPath, "w", newline='') as datafile:
+    # Write all the lines to the text file
     datafile.writelines("Financial Analysis\n")
 
     datafile.writelines("---------------------\n")
@@ -59,8 +73,10 @@ with open(outputPath, "w", newline='') as datafile:
     datafile.writelines(f"Greatest Increase in Profits: {greatestIncDate} (${greatestInc})\n")
     datafile.writelines(f"Greatest Decrease in Profits: {greatestDecDate} (${greatestDec})\n")
 
+    # Close the output file
     datafile.close()
 
+    # Print results to the console
     print(f"Financial Analysis")
 
     print(f"---------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,51 +1,68 @@
 import os
 import csv
 
-from collections import Counter
-
+# Specify the source data file
 csvpath = os.path.join('Resources/election_data.csv')
 
+# Open the data source
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
 
+    # Read past the header row
     csv_header = next(csvreader)
-
+    
+    # Create a variable to store the total votes cast
     totalVotes = 0
 
+    # Create a list to store the candidate names
     candidates = []
+
+    # Create a dictionary to store the candidate names and votes
     candidateVotes = {}
 
+    # Iterate through each row
     for row in csvreader:
+        # Increment the total vote value
         totalVotes += 1
 
+        # Store the candidate value in a vairable
         candidate = row[2]
 
+        # Check if the candidate is in the list of candidates
         if(candidate in candidates):
+            # If so, find them in the dictionary and increment their total votes by 1
             candidateVotes[candidate] += 1
         else:
+            # If not, add the candidate to the list of candidates and add them to the dictionary with a vote of 1
             candidates.append(candidate)
             candidateVotes[candidate] = 1
 
-
+# Create a varaible to store the winner and top vote count
 winner = ""
 winningVote = 0
 
+# Create the output file path
 outputPath = os.path.join("analysis/output.txt")
 
 with open(outputPath, "w", newline='') as datafile:
 
+    # Print the results to the console and write them to the file at the same time
     print(f"Election Results")
     datafile.writelines(f"Election Results\n")
+
     print(f"---------------------")
     datafile.writelines(f"---------------------\n")
 
+    # For each candidate in the list, find how many votes they got to list
     for c in candidates:
         votes = candidateVotes.get(c)
-
+        
+        # Calculate the winner of the votes
         if(votes > winningVote):
             winner = c
             winningVote = votes
 
+        # Calculate the % of total votes a candidate received and round to 3 decimals
         votePer = round((votes / totalVotes)*100, 3)
 
         print(f"{c}: {votePer}% ({votes})")
